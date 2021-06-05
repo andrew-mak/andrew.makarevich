75889cfec2f07f4a7fa5bbb69218805adb3da545
Andrei <andrews.makarevich@gmail.com>
2021-06-05 20:07:00 +0300

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/js/app.js',
+  entry: './src/index.js',
   mode: 'development',
   output: {
-    path: `${__dirname}/public`,
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: 'assets/img/[hash:8][ext][query]',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          sources: {
+            list: [
+              // All default supported tags and attributes
+              '...',
+              {
+                tag: 'img',
+                attribute: 'data-src',
+                type: 'src',
+              },
+              {
+                tag: 'img',
+                attribute: 'data-srcset',
+                type: 'srcset',
+              },
+              {
+                tag: 'link',
+                attribute: 'href',
+                type: 'src',
+                filter: (tag, attribute, attributes, resourcePath) => {
+                  if (!/stylesheet/i.test(attributes.rel)) {
+                    return false;
+                  }
+                  return true
+                },
+              },
+            ],
+          }
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|woff|ttf)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+  ]
 };
